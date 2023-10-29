d45748e7b46252adae3ef2da4ed88f9f29a405bc
saranshisatgit <saranshisatgit>
2023-10-29 16:31:41 +0100

    second commit with fixes

--- sign.py
+++ sign.py
@@ -7,7 +7,7 @@ from PyPDF2 import PdfReader
 from utils import filter_by_proximity
 
 
-def sign_pdf(input_pdf_path, temp_pdf_path, output_pdf_path, signatures, identifiers, offset_increment=40):
+def sign_pdf(input_pdf_path, temp_pdf_path, output_pdf_path, signatures, identifiers, offset_increment=40, check_proximity=False, adjust_coords=False, reference_text_distance=30, spacing=5):
     # Copy the original PDF to a temporary PDF
     shutil.copy(input_pdf_path, temp_pdf_path)
     # Determine the number of pages in the PDF
@@ -16,17 +16,15 @@ def sign_pdf(input_pdf_path, temp_pdf_path, output_pdf_path, signatures, identif
         num_pages = len(pdf.pages)
 
     if num_pages > 2:
-        print('Multiple Pages PDF for Signing')
-        proximity = False
-        with_white_space = False
-        if proximity:
-            coordinates_dict = modified_extraction_with_proximity_check(temp_pdf_path, identifiers)  
-        if not proximity and not with_white_space:
-            coordinates_dict = refined_multi_page_get_text_coordinates(temp_pdf_path, identifiers)
-            coordinates_dict = adjust_signature_coordinates(coordinates_dict, 400, 200)
-            
+        coordinates_dict = modified_extraction_with_proximity_check(temp_pdf_path, identifiers)
+    if not any(coordinates_dict.values()):
+        coordinates_dict = refined_multi_page_get_text_coordinates(temp_pdf_path, identifiers)
+
+    if adjust_coords and check_proximity:
+        coordinates_dict = adjust_signature_coordinates(coordinates_dict, reference_text_distance, spacing)
+    
     else:
-        # Using the modified get_text_coordinates function
+        # End up sing this its better
         coordinates_dict = get_text_coordinates(temp_pdf_path, identifiers)
 
     print('Coordinates found for,', coordinates_dict)
@@ -47,11 +45,16 @@ def sign_pdf(input_pdf_path, temp_pdf_path, output_pdf_path, signatures, identif
     os.remove(temp_pdf_path)
 
 def main():
-    parser = argparse.ArgumentParser(description="Automatically sign PDF files.")
-    parser.add_argument("input_pdf", type=str, help="Path to the input PDF file.")
-    parser.add_argument("output_pdf", type=str, help="Path to the output signed PDF file.")
-    parser.add_argument("--signatures", type=str, nargs='+', required=True, help="Path(s) to the signature image file(s).")
-    parser.add_argument("--identifiers", type=str, nargs='+', required=True, help="Identifier text(s) where the signature needs to be placed.")
+    parser = argparse.ArgumentParser(description="Automatically sign PDFs at specified identifiers' locations.")
+    parser.add_argument("--input_pdf", required=True, help="Path to the input PDF.")
+    parser.add_argument("--output_pdf", required=True, help="Path to the output signed PDF.")
+    parser.add_argument("--signatures", required=True, nargs='+', help="List of paths to signature images.")
+    parser.add_argument("--identifiers", required=True, nargs='+', help="List of identifier texts in the PDF where signatures should be placed.")
+    parser.add_argument("--check_proximity", action="store_true", help="Enable proximity check to refine signature placement.")
+    parser.add_argument("--adjust_coords", action="store_true", help="Adjust signature coordinates if they are too close.")
+    parser.add_argument("--reference_text_distance", type=int, default=30, help="Reference distance to check proximity of text blocks (default: 30).")
+    parser.add_argument("--spacing", type=int, default=5, help="Spacing to be used between signatures if they are too close (default: 5).")
+
     
     args = parser.parse_args()
 
@@ -60,7 +63,7 @@ def main():
         return
     
     temp_pdf = "temp_output.pdf"
-    sign_pdf(args.input_pdf, temp_pdf,  args.output_pdf, args.signatures, args.identifiers)
+    sign_pdf(args.input_pdf, temp_pdf,  args.output_pdf, args.signatures, args.identifiers, args.check_proximity, args.adjust_coords, args.reference_text_distance, args.spacing)
 
 if __name__ == "__main__":
     main()
